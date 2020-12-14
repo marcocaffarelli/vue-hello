7bbaa09cfe552c518b3178034b0e309cab1e3856
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-14 14:11:49 +0100

    Utilizzando i data stampato un messaggio in un h1
    Utilizzando i data stampato un messaggio in un h1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere un’immagine presa anch’essa da un data
 
+
+//V-Model
 let app = new Vue({
     //option object
     el: "#app",   
     data:{
-        
+        messaggio: "Primo esercizio con Vue"
     } 
 });
