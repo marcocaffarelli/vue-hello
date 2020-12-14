3e9c811da9816b10092eb29d5face083266862f6
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-14 14:00:27 +0100

    Collegato il file js all'HTML
    Collegato il file js all'HTML

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Istruzioni:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+
+let app = new Vue({
+    //option object
+    el: "#app",   
+    data:{
+        
+    } 
+});
